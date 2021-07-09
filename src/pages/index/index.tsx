9d5e0f2b26ce052c57ce3a8f7f8397c4c12f674b
boxcc <bob.zhuang@kiwifi.cn>
2021-07-09 22:19:02 +0800

    fix: 测试下滑刷新

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from '@tarojs/components'
 import { AtButton } from 'taro-ui'
 import { useQuery } from 'react-query'
 import { GraphQLClient, gql } from 'graphql-request'
-import Taro from '@tarojs/taro'
+import Taro, { usePullDownRefresh } from '@tarojs/taro'
 
 import 'taro-ui/dist/style/components/button.scss' // 按需引入
 import './index.scss'
@@ -11,31 +11,34 @@ import './index.scss'
 interface Props {}
 
 const Index: React.FC<Props> = () => {
-  const { data, isFetching, error } = useQuery('posts', async () => {
-    const graphQLClient = new GraphQLClient('http://localhost:1337/graphql', {
-      fetch: (url, options) =>
-        new Promise(resolve =>
-          Taro.request({
-            url,
-            header: options.headers,
-            method: options.method,
-            data: options.body,
-            success: res =>
-              resolve({
-                headers: {
-                  ...options.headers,
-                  get: (key: string) => options.headers[key]
-                },
-                ok: res.statusCode >= 200 && res.statusCode < 300,
-                statusText: () => res.errMsg,
-                json: () => Promise.resolve(res.data)
-              }),
-            fail: res => {
-              console.log(`res`, res)
-            }
-          })
-        )
-    })
+  const { data, isFetching, error, refetch } = useQuery('posts', async () => {
+    const graphQLClient = new GraphQLClient(
+      'http://192.168.68.125:1337/graphql',
+      {
+        fetch: (url, options) =>
+          new Promise(resolve =>
+            Taro.request({
+              url,
+              header: options.headers,
+              method: options.method,
+              data: options.body,
+              success: res =>
+                resolve({
+                  headers: {
+                    ...options.headers,
+                    get: (key: string) => options.headers[key]
+                  },
+                  ok: res.statusCode >= 200 && res.statusCode < 300,
+                  statusText: () => res.errMsg,
+                  json: () => Promise.resolve(res.data)
+                }),
+              fail: res => {
+                console.log(`res`, res)
+              }
+            })
+          )
+      }
+    )
     const result = await graphQLClient.request(
       gql`
         query {
@@ -57,6 +60,11 @@ const Index: React.FC<Props> = () => {
   console.log(`data`, data)
   console.log(`isFetching`, isFetching)
   console.log(`error`, error)
+
+  usePullDownRefresh(() => {
+    console.log('onPullDownRefresh')
+    refetch()
+  })
 
   return (
     <View className='index'>
